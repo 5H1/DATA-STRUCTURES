df033fc648bd2a4a0e1c32302ad33e06e1d664bf
5H1 <59143631+5H1@users.noreply.github.com>
2020-08-31 18:19:12 +0530

    demo file

---
+++ demo.java
@@ -0,0 +1,10 @@
+import java.io.*;
+public class Demo{
+         public static void main(String args[ ])[
+                   int a=10;
+                   System.out.println("HELLO WORLD");
+                   System.out.print("HI");
+                   System.out.print("JAVA INTRODUCTION");
+                   System.out.println("a=%d",a);
+                   }
+}
